f667fc20530c3524fee803c7766557b1c7faa81a
iiBlackwolf <blackwolf3466@gmail.com>
2023-04-30 20:51:38 +0300

    Simple http server

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 const http = require('http');
 
+let users = [
+  {name:"Joe", age:30, city:"New York"},
+  {name:"Rick", age:48, city:"London"}
+]
+
 const port = 8080
+
+async function handleGetRequest(req, res) {
+  const { pathname } = new URL(req.url, `http://${req.headers.host}`);
+  let data = [];
+
+  switch (pathname) {
+    case '/':
+      return res.end('Server is running...');
+    case '/users':
+      data.push(users);
+      res.setHeader('Content-Type', 'application/json');
+      return res.end(JSON.stringify(data));
+    default:
+      res.statusCode = 404;
+      return res.end('404, Requested resource does not exist');
+  }
+}
+
 const server = http.createServer((req, res) => {
-  res.end("Ok");
+  
+  if (req.method === 'GET') {
+    handleGetRequest(req, res);
+  }
+
 })
 
 server.listen(port, () => {
-  console.log('Listening on:\n Localhost: ' + 'http://localhost:');
+  console.log('Listening on:\n Localhost: ' + 'http://localhost:' + port);
 })
